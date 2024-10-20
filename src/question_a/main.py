1308c9080922642f6bf40fa5a70d91f7217c296d
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2024-10-20 00:04:11 +0000

    question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,8 @@
 #add import
+import question_a
+from question_a.question_a import get_miles_per_hour
+
+def main():
+    question_a.get_miles_per_hour()
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,23 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def get_miles_per_hour (kilometers, minutes):
+    if kilometers < 0 or minutes < 0:
+        return 'Invalid arguments'
+    
+    conv = 0.621371
+    miles = kilometers * conv
+    
+    hours = min /60
+    miles_per_hour = miles / hours
+
+    return miles_per_hour
+
+if __name__ == "__main__":
+    kilometers = int(input("Enter Kilometers: "))
+    minutes = int(input("Enter Minutes: "))
+
+    result = get_miles_per_hour(kilometers, minutes)
+
+    print (result)
